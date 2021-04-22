f2c5711748fb3c10b9d366a16a482459f8c9eba9
Soberat <soberat1@gmail.com>
2021-04-22 13:03:36 +0200

    Add public functions, including related constants and dictionaries

--- Controller.py
+++ Controller.py
@@ -10,12 +10,12 @@ from datetime import datetime
 # TODO: Timed dosing functions
 # TODO: public functions to be used by the GUI
 # TODO: checksum verification
+# TODO: Implement positive/negative acknowledgements
 # Class representing a single Brooks 4850 Mass Flow Controller,
 # Handling communication according to the datasheets
 
 
 class Controller:
-
     # Process value codes
     PARAM_PV_MEASURE_UNITS = 0x04
     PARAM_PV_TIME_BASE = 0x0A
@@ -32,6 +32,104 @@ class Controller:
     PARAM_SP_BATCH = 0x2C
     PARAM_SP_BLEND = 0x2D
     PARAM_SP_SOURCE = 0x2E
+
+    # Valve override states
+    VOR_OPTION_NORMAL = 0
+    VOR_OPTION_CLOSED = 1
+    VOR_OPTION_OPEN = 2
+
+    # Setpoint functions
+    SP_FUNC_RATE = 1
+    SP_FUNC_BATCH = 2
+    SP_FUNC_BLEND = 3
+
+    # Setpoint sources
+    SP_SOURCE_KEYPAD = 0
+    SP_SOURCE_SERIAL = 1
+
+    # Input port types
+    # type : internal code dictionary
+    INPUT_PORT_TYPES = {
+        "Off": 0,
+        "0-20mA": 7,
+        "4-20mA": 8,
+        "0-10V": 9,
+        "2-10V": ":",
+        "0-5V": ';',
+        "1-5V": '<'
+    }
+
+    DECIMAL_POINTS = {
+        "xxx.": 0,
+        "xx.x": 1,
+        "x.xx": 2,
+        ".xxx": 3
+    }
+
+    # Output port types
+    OUTPUT_PORT_TYPES = {
+        "Off": 0,
+        "0-20mA": 1,
+        "4-20mA": 2,
+        "0-10V": 3,
+        "2-10V": 4,
+        "0-5V": 5,
+        "1-5V": 6
+    }
+
+    # Measurement units
+    MEASUREMENT_UNITS = {
+        "ml": 0,
+        "mls": 1,
+        "mln": 2,
+        "l": 3,
+        "ls": 4,
+        "ln": 5,
+        "cm^3": 6,
+        "cm^3s": 7,
+        "cm^3n": 8,
+        "m^3": 9,
+        "m^3s": 10,
+        "m^3n": 11,
+        "g": 12,
+        "lb": 13,
+        "kg": 14,
+        "ft^3": 15,
+        "ft^3s": 16,
+        "ft^3n": 17,
+        "scc": 18,
+        "sl": 19,
+        "bar": 20,
+        "mbar": 21,
+        "psi": 22,
+        "kPa": 23,
+        "Torr": 24,
+        "atm": 25,
+        "Volt": 26,
+        "mA": 27,
+        "oC": 28,
+        "oK": 29,
+        "oR": 30,
+        "oF": 31,
+        "g/cc": 32,
+        "sg": 33,
+        "%": 34,
+        "lb/in^3": 35,
+        "lb/ft^3": 36,
+        "lb/gal": 37,
+        "kg/m^3": 38,
+        "g/ml": 39,
+        "kg/l": 40,
+        "g/l": 41
+    }
+
+    # Base time units
+    RATE_TIME_BASE = {
+        "sec": 1,
+        "min": 2,
+        "hrs": 3,
+        "day": 4
+    }
 
     # Response codes - not very well documented, so the meaning is assumed
     RESPONSE_OK = '4'
@@ -79,6 +177,7 @@ class Controller:
     # This is an internal write functions to be used by the public functions
     # Returns whatever was written to the variable, None if some error occurred
     def __write_value(self, param, value):
+        # The only difference for writing is the input or output port, which are addressed differently
         if param == Controller.PARAM_SP_FUNCTION or param == Controller.PARAM_SP_RATE or param == Controller.PARAM_SP_VOR or param == Controller.PARAM_SP_BATCH or param == Controller.PARAM_SP_BLEND or param == Controller.PARAM_SP_SOURCE:
             # Create and send ascii encoded command via serial, wait for response
             command = f'AZ.{self.__outputPort}P{param}={value}\r'
@@ -96,6 +195,50 @@ class Controller:
                 return response[4]
         else:
             return None
+
+    # Sets the maximum possible rate in reference to control signal
+    def set_gas_factor(self, value):
+        assert (-999.999 <= value <= 999.999)  # Possible setpoint values according to the datasheet (section C-5-4)
+        value = int(value * 1000)  # value is written to serial as XXXXXX without the decimal
+        return self.__write_value(Controller.PARAM_PV_GAS_FACTOR, value)
+
+    # Public function to set the head operation point (setpoint)
+    def set_setpoint(self, value):
+        assert (-999.999 <= value <= 999.999)  # Possible setpoint values according to the datasheet (section C-5-4)
+        value = int(value*1000)  # value is written to serial as XXXXXX without the decimal
+        return self.__write_value(Controller.PARAM_SP_RATE, value)
+
+    # Sets the setpoint function, rate/batch/blend.
+    # Plan is to only use rate.
+    def set_function(self, function):
+        assert(function == Controller.SP_FUNC_RATE or function == Controller.SP_FUNC_BATCH or function == Controller.SP_FUNC_BLEND)
+        return self.__write_value(Controller.PARAM_SP_FUNCTION, function)
+
+    # Sets the maximum possible rate in reference to control signal
+    def set_full_scale(self, value):
+        assert (-999.999 <= value <= 999.999)  # Possible setpoint values according to the datasheet (section C-5-4)
+        value = int(value * 1000)  # value is written to serial as XXXXXX without the decimal
+        return self.__write_value(Controller.PARAM_PV_FULL_SCALE, value)
+
+    # Public function to control manual valve override option
+    def set_valve_override(self, state):
+        assert (state == Controller.VOR_OPTION_NORMAL or state == Controller.VOR_OPTION_CLOSED or state == Controller.VOR_OPTION_OPEN)
+        return self.__write_value(Controller.PARAM_SP_VOR, state)
+
+    # Batch/blend ratios setting
+    def set_batch(self, value):
+        assert (-999.999 <= value <= 999.999)  # Possible setpoint values according to the datasheet (section C-5-4)
+        value = int(value * 1000)  # value is written to serial as XXXXXX without the decimal
+        return self.__write_value(Controller.PARAM_SP_BATCH, value)
+
+    def set_blend(self, value):
+        assert (-999.999 <= value <= 999.999)  # Possible setpoint values according to the datasheet (section C-5-4)
+        value = int(value * 1000)  # value is written to serial as XXXXXX without the decimal
+        return self.__write_value(Controller.PARAM_SP_BLEND, value)
+
+    def set_source(self, source):
+        assert(source == Controller.SP_SOURCE_SERIAL or source == Controller.SP_SOURCE_KEYPAD)
+        return self.__write_value(Controller.PARAM_SP_SOURCE, source)
 
     # Save samples to a csv file, named after the current time and controller number it is coming from
     def save_readouts(self):
