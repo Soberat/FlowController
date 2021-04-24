c3c1664c2fc753eeb401de7b9e92cda818917c25
Soberat <soberat1@gmail.com>
2021-04-24 11:51:53 +0200

    Clean up main.py to prepare for gui code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import serial
 from serial.tools.list_ports import comports
 
-# This code is to be reused by the GUI later on
-
 # communication parameters from datasheet
 serialCon = serial.Serial(baudrate=9600,
                           parity=serial.PARITY_NONE,
@@ -10,48 +8,3 @@ serialCon = serial.Serial(baudrate=9600,
                           bytesize=serial.EIGHTBITS)
 # setting the COM port after the constructor prevents automatic attempts at connecting
 serialCon.port = 'COM3'
-
-# COM port parameters
-__deviceId = ""
-__deviceName = ""
-
-# USB device info - unknown if needed
-__deviceVid = 0
-__devicePid = 0
-__deviceSN = ""
-__deviceLoc = ""
-__deviceManufacturer = ""
-__deviceModel = ""
-__deviceInterface = ""
-
-# function that opens the serial port communication and configures anything else that's required
-# TODO: should set the setpoint source and initial setpoint, get and set COM/USB parameters,
-#  get gas parameters, possibly more
-def open(self):
-    if not self.__serial.is_open:
-        self.__serial.open()
-    else:
-        print("Tried to open port {cNum} when it was opened".format(cNum=self.__controllerNumber))
-    return self.__serial.is_open
-
-
-# TODO: Should reverse what open() did - set the setpoint control to voltage, possibly more
-def close(self):
-    if self.__serial.is_open:
-        self.__serial.close()
-    else:
-        print("Tried to close port {cNum} when it was closed".format(cNum=self.__controllerNumber))
-    return self.__serial.is_open
-
-
-# We assume the 'port' argument is taken from ListPortInfo.name.
-# This should preserve compatibility between operating systems
-# Even though the function checks if the serial port is already open,
-# the GUI should also prevent the users form changing the port while it's open.
-def change_port(self, port):
-    if not self.__serial.is_open:
-        self.__serial.port = port
-
-
-def get_com_ports():
-    return list(comports())
