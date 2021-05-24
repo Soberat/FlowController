d724e7dbcd945827590473fa844538139777a0f7
Soberat <soberat1@gmail.com>
2021-05-24 12:52:24 +0200

    Update Sensor class
    -Open connection upon creation of serial object by passing comport as constructor parameter,
    -Remove datalen member (no longer needed),
    -Increase timeout to a small amount (0 caused readline() to return nothing)
    -Add 'command' member to configure what program has to write to the sensor host to acquire data,
    -Use readline() to correctly read data

--- Sensor.py
+++ Sensor.py
@@ -9,16 +9,16 @@ from collections import deque
 # over a serial connection. Optional header will be added to saved csv file
 class Sensor:
     def __init__(self, comport='COM1', baudrate=9600, parity=serial.PARITY_NONE, databits=serial.EIGHTBITS,
-                 stopbits=serial.STOPBITS_ONE, dataHeader='', bufferSize=64, datalen=32):
+                 stopbits=serial.STOPBITS_ONE, dataHeader='', bufferSize=64, command=":MEAS?"):
         self.__serial = serial.Serial(baudrate=baudrate,
                                       parity=parity,
                                       bytesize=databits,
                                       stopbits=stopbits,
-                                      timeout=0)
-        self.__serial.port = comport
-        self.__datalen = datalen
+                                      timeout=100,
+                                      port=comport)
         self.header = dataHeader
         self.buffer = deque(maxlen=bufferSize)
+        self.command = command
 
     # Open/close serial connection
     def open(self):
@@ -43,9 +43,10 @@ class Sensor:
     # It assumes that data is passed as a newline-terminated string, and the interpretation is up to the user
     def getData(self):
         assert self.__serial.is_open
-        buffer = self.__serial.read(self.__datalen)
-        if len(buffer) > 0:
-            self.buffer.append(buffer.decode('ascii'))
+        self.__serial.write(f"{self.command}\n".encode())
+        response = self.__serial.readline().decode('utf-8')
+        if len(response) > 0:
+            self.buffer.append(response)
 
     # function to change the amount of stored samples without losing previously gathered samples
     def change_buffer_size(self, value):
